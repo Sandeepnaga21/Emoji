6ff1f442bc1e62389218e16f07d702c5ddd5ffb2
sandeep <sandeepnagapeteru121@gamail.com>
2023-12-25 10:45:03 +0000

    solution

---
+++ src/components/EmojiGame/index.js
@@ -0,0 +1,28 @@
+import {Component} from 'react'
+import EmojiCard from '..EmojiCard'
+import NavBar from '../NavBar'
+import '.index.css'
+
+class EmojiGame extends Component {
+  state = {count: 0, clickedEmojisList: []}
+
+  shuffledEmojisList = () => {
+    const {emojisList} = this.props
+    return emojisList.sort(() => Math.random() - 0.5)
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <NavBar count={count} />
+        <ul className="emoji-card">
+          {shuffledEmojisList.map(eachOne => (
+            <EmojiCard emojiDetails={eachOne} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default EmojiGame
